da98788c48bb09682d229c9c7fb2c9a4bce66d53
Eric Bakan <eric@ebakan.com>
2013-01-24 18:02:55 -0500

    Cache coefficients for increased performance

--- takktile-arduino.ino
+++ takktile-arduino.ino
@@ -1,52 +1,12 @@
 #include <Wire.h>
+#define NUM_SENSORS 5
+float a0[NUM_SENSORS];
+float b1[NUM_SENSORS];
+float b2[NUM_SENSORS];
+float c12[NUM_SENSORS];
 
-void setup () {
-  Wire.begin();
-  Serial.begin(9600);
-}
-void readNum(byte num, float* oTemp, float* oPressure)
-{
-  // Select sensor
-  Wire.beginTransmission(0x30+num);
-  Wire.endTransmission();
-
-  // Request coefficients
-  Wire.beginTransmission(0x60);
-  Wire.write(0x04);
-  Wire.endTransmission();
-  Wire.requestFrom(0x60, 8);
-  int16_t a0coeff = (( (uint16_t) Wire.read() << 8) | Wire.read());
-  int16_t b1coeff = (( (uint16_t) Wire.read() << 8) | Wire.read());
-  int16_t b2coeff = (( (uint16_t) Wire.read() << 8) | Wire.read());
-  int16_t c12coeff = (( (uint16_t) (Wire.read() << 8) | Wire.read())) >> 2;
-  float _mpl115a2_a0 = (float)a0coeff / 8;
-  float _mpl115a2_b1 = (float)b1coeff / 8192;
-  float _mpl115a2_b2 = (float)b2coeff / 16384;
-  float _mpl115a2_c12 = (float)c12coeff;
-  _mpl115a2_c12 /= 4194304.0;
-
-  // Request raw sensor measurements
-  Wire.beginTransmission(0x60);
-  Wire.write(0x00);
-  Wire.endTransmission();
-
-  Wire.requestFrom(0x60, 4);
-  uint16_t pressure = (( (uint16_t) Wire.read() << 8) | Wire.read()) >> 6;
-  uint16_t temp = (( (uint16_t) Wire.read() << 8) | Wire.read()) >> 6;
-
-  float pressureComp = _mpl115a2_a0 + (_mpl115a2_b1 + _mpl115a2_c12 * temp ) * pressure + _mpl115a2_b2 * temp;
-
-  // Turn sensor off
-  Wire.requestFrom(0x30+num, 1);
-
-  // Calculate temp & pressure
-  *oPressure = ((65.0F / 1023.0F) * pressureComp) + 50.05F; // kPa
-  *oTemp = ((float) temp - 498.0F) / -5.35F + 25.0F; // C
-  //*oPressure = pressure;
-  //*oTemp = temp;
-}
-void loop() {
-  // s 0C
+void initialize() {
+    // s 0C
   Wire.beginTransmission(0x06);
   Wire.endTransmission();
   
@@ -60,8 +20,71 @@ void loop() {
   Wire.requestFrom(0x06, 1);
   
   delay(5);
+}
+
+void readCoeffs(byte num) {
   
-  for(int i=0;i<5;i++)
+  // Select sensor
+  Wire.beginTransmission(0x30+num);
+  Wire.endTransmission();
+  
+  // Request coefficients
+  Wire.beginTransmission(0x60);
+  Wire.write(0x04);
+  Wire.endTransmission();
+  Wire.requestFrom(0x60, 8);
+  int16_t a0coeff = (( (uint16_t) Wire.read() << 8) | Wire.read());
+  int16_t b1coeff = (( (uint16_t) Wire.read() << 8) | Wire.read());
+  int16_t b2coeff = (( (uint16_t) Wire.read() << 8) | Wire.read());
+  int16_t c12coeff = (( (uint16_t) (Wire.read() << 8) | Wire.read())) >> 2;
+  // Turn sensor off
+  Wire.requestFrom(0x30+num, 1);
+  
+  a0[num] = (float)a0coeff / 8;
+  b1[num] = (float)b1coeff / 8192;
+  b2[num] = (float)b2coeff / 16384;
+  c12[num] = (float)c12coeff;
+  c12[num] /= 4194304.0;
+}
+
+void setup () {
+  Wire.begin();
+  Serial.begin(9600);
+  initialize();
+  for(int i=0;i<NUM_SENSORS;i++) {
+    readCoeffs(i);
+  }
+}
+void readNum(byte num, float* oTemp, float* oPressure)
+{
+  // Select sensor
+  Wire.beginTransmission(0x30+num);
+  Wire.endTransmission();
+
+  // Request P/T data
+  Wire.beginTransmission(0x60);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  Wire.requestFrom(0x60, 4);
+  uint16_t pressure = (( (uint16_t) Wire.read() << 8) | Wire.read()) >> 6;
+  uint16_t temp = (( (uint16_t) Wire.read() << 8) | Wire.read()) >> 6;
+  
+  // Turn sensor off
+  Wire.requestFrom(0x30+num, 1);
+
+  float pressureComp = a0[num] + (b1[num] + c12[num]  * temp)  * pressure + b2[num] * temp;
+
+  // Calculate temp & pressure
+  *oPressure = ((65.0F / 1023.0F) * pressureComp) + 50.05F; // kPa
+  *oTemp = ((float) temp - 498.0F) / -5.35F + 25.0F; // C
+  //*oPressure = pressure;
+  //*oTemp = temp;
+}
+void loop() {
+  initialize();
+  
+  for(int i=0;i<NUM_SENSORS;i++)
   {
     float oTemp, oPressure;
     readNum(i, &oTemp, &oPressure);
